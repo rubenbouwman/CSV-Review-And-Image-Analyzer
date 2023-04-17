d499bf556221c349ab6043bb70418e086e6cac38
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-17 19:25:01 +0200

    Update Image-color-analyzer.py
    Updated comments and changed variables

--- src/Image-color-analyzer.py
+++ src/Image-color-analyzer.py
@@ -2,7 +2,7 @@
 # https://medium.com/codex/rgb-to-color-names-in-python-the-robust-way-ec4a9d97a01f
 
 import os
-from colorthief import ColorThief # >>> pip install colorthief <<<
+from colorthief import ColorThief
 from scipy.spatial import KDTree
 from webcolors import (CSS3_HEX_TO_NAMES, hex_to_rgb,)
 import timeit
@@ -10,21 +10,23 @@ import timeit
 # -------------------- Variables --------------------
 ColorCodes = []
 ColorNames = []
-# a dictionary of all the hex and their respective names in css3
-css3_db = CSS3_HEX_TO_NAMES
-names = []
+
+namesDictionary = []
 rgb_values = []
 # -------------------- Methods --------------------
+# Fills the namesDictionary and rgb_values lists with the contents from the css3 dictionary
 def createColorNames():
+    # a dictionary of all the hex and their respective names in css3
+    css3_db = CSS3_HEX_TO_NAMES
     for color_hex, color_name in css3_db.items():
-        names.append(color_name)
+        namesDictionary.append(color_name)
         rgb_values.append(hex_to_rgb(color_hex))
 
 # This method assigns a matching name to the given RGB color code
 def colorToText(rgb_tuple):
     kdt_db = KDTree(rgb_values)
     distance, index = kdt_db.query(rgb_tuple)
-    return names[index]
+    return namesDictionary[index]
 
 # returns the RGB color code of the most dominant color within the image
 def getDominantColor(filename):
