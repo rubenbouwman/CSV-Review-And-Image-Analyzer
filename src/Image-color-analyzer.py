91fb78d1446944045ff554d9822defdd48ee0b1d
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-17 14:41:44 +0200

    Made an analyzeColors method
    analyses the dominant color within an image and saves it to a list

---
+++ src/Image-color-analyzer.py
@@ -0,0 +1,17 @@
+import os
+from colorthief import ColorThief # >>> pip install colorthief <<<
+# -------------------- Variables --------------------
+Colors = []
+
+# -------------------- Methods --------------------
+def analyzeColors():
+    print('Analyzing...')
+    for filename in os.listdir('Images'):
+        color_thief = ColorThief(f'Images/{filename}')
+        # get the dominant color
+        dominant_color = color_thief.get_color(quality=1)
+        print(f'File: {filename} >>> Dominant color: {dominant_color}')
+        Colors.append(dominant_color)
+
+# -------------------- run --------------------
+analyzeColors()
--- src/csv-analyzer.py
+++ src/csv-analyzer.py
@@ -6,3 +6,4 @@ data = read_csv("CSV Files/Bol-product-reviews.csv") # Path to your CSV File
 # converting column data to list
 # Change the string within [] to the collumn name containing the reviews
 reviews = data['score'].tolist()
+
