e26c41d9bd03e32ee3f24cf41d4a6dba25c18f81
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-19 03:05:28 +0200

    Delete csv-analyzer.py

--- src/csv-analyzer.py
+++
@@ -1,9 +0,0 @@
-from pandas import *
-
-# Reading CSV file
-data = read_csv("CSV Files/Bol-product-reviews.csv") # Path to your CSV File
-
-# converting column data to list
-# Change the string within [] to the collumn name containing the reviews
-reviews = data['score'].tolist()
-
